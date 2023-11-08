b772e0533c9ed76c68df977c925d3696d7bb57af
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2023-11-08 08:41:55 -0300

    update

--- private/assets/linkage.js
+++ private/assets/linkage.js
@@ -3,10 +3,14 @@ const { encoding } = require("../config.json");
 const { JSDOM } = require("jsdom");
 const path = require("path");
 const fs = require("fs");
+const { normalize, join } = require("path");
+
+let domIndex, domPage;
+let docIndex, docPage;
+const attLink = ["link", "link-processed", "link-remove"];
 
 function htmlLinkage(fName = "") {
     const fPath = path.join(dApp, "page", fName);
-    let domIndex, domPage;
 
     try {
         domIndex = new JSDOM(fs.readFileSync(path.join(dApp, "base.html"), { encoding }));
@@ -16,43 +20,61 @@ function htmlLinkage(fName = "") {
         console.error(error);
         return false;
     }
+    docIndex = domIndex.window.document, docPage = domPage.window.document;
 
-    const docIndex = domIndex.window.document, docPage = domPage.window.document;
-
-    const links = docIndex.querySelectorAll("[link]:not([link] [link])");
+    let links = docIndex.querySelectorAll("[link]:not([link] [link])");
     for (let elLink of links) carregarElemento(elLink);
 
-    function carregarElemento(elIndex) {
-        if (!elIndex) return;
-        if (elIndex.getAttribute("link-processed") === "true") return;
-        elIndex.setAttribute("link-processed", "true");
+    let elTabs = docIndex.querySelector("tab-list");
+    if (elTabs) tabsCreate(elTabs);
 
-        const nm = elIndex.getAttribute("link");
-        if (!nm) return;
-        const elPage = docPage.querySelector(nm);
-        if (elPage == null) return;
-
-        const ic = elPage.hasAttribute("link-remove") ? elPage.getAttribute("link-remove") : elIndex.getAttribute("link-remove");
-        if (ic === 'true') {
-            elIndex.remove();
-            elPage.remove();
-            return;
-        }
-
-        let childs = elIndex.querySelectorAll("[link]");
-        childs.forEach(el => carregarElemento(el));
-
-        for (let i = 0; i < elPage.attributes.length; i++) {
-            const attribute = elPage.attributes[i];
-            elIndex.setAttribute(attribute.name, attribute.value);
-        }
-        elIndex.innerHTML = elIndex.innerHTML + elPage.innerHTML;
-        elPage.remove();
-    }
+    links = docIndex.querySelectorAll("[" + attLink.join("], [") + "]");
+    for (let el of links) for (let att of attLink) el.removeAttribute(att);
 
     let strContent = domIndex.serialize();
     strContent = strContent.replace(/FILENAME/g, fName.replace(".html", ""));
     return fCreate(fName, dBuild, strContent);
+}
+
+function carregarElemento(elIndex) {
+    if (!elIndex) return;
+    if (elIndex.getAttribute("link-processed") === "true") return;
+    elIndex.setAttribute("link-processed", "true");
+
+    const nm = elIndex.getAttribute("link");
+    if (!nm) return;
+    const elPage = docPage.querySelector(nm);
+    if (elPage == null) return;
+
+    const ic = elPage.hasAttribute("link-remove") ? elPage.getAttribute("link-remove") : elIndex.getAttribute("link-remove");
+    if (ic === 'true') {
+        elIndex.remove();
+        elPage.remove();
+        return;
+    }
+
+    let childs = elIndex.querySelectorAll("[link]");
+    childs.forEach(el => carregarElemento(el));
+
+    for (let i = 0; i < elPage.attributes.length; i++) {
+        const attribute = elPage.attributes[i];
+        elIndex.setAttribute(attribute.name, attribute.value);
+    }
+    elIndex.innerHTML = elIndex.innerHTML + elPage.innerHTML;
+    elPage.remove();
+}
+
+function tabsCreate(el) {
+    let tabs = el.getAttribute("tabs");
+    if (!tabs) return;
+    tabs = tabs.split("+");
+
+    for (let nmTab of tabs) {
+        const tab = docIndex.createElement("tab");
+        tab.setAttribute("href", normalize(nmTab));
+        tab.innerText = nmTab;
+        el.appendChild(tab);
+    }
 }
 
 module.exports = htmlLinkage;
